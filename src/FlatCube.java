d225d441da88dfc9c220015f11235ebc13129de5
Nayeon <kimnayeon0108@users.noreply.github.com>
2020-12-07 19:02:29 +0900

    Add set direction function

--- src/FlatCube.java
+++ src/FlatCube.java
@@ -1,47 +1,92 @@
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class FlatCube {
     private char[][] cube = {{'R', 'R', 'W'},
-                        {'G', 'C', 'W'},
-                        {'G', 'B', 'B'}};
+            {'G', 'C', 'W'},
+            {'G', 'B', 'B'}};
     private String input;
+    private boolean direction;
     private Scanner s = new Scanner(System.in);
 
-    public FlatCube(){
+    public FlatCube() {
         printCube();
         move();
     }
 
-    private void printCube(){
-        for(int i = 0; i < cube.length; i++){
-            for(int j = 0; j < cube[i].length; j++){
+    private void printCube() {
+        for (int i = 0; i < cube.length; i++) {
+            for (int j = 0; j < cube[i].length; j++) {
                 System.out.print(cube[i][j] + " ");
             }
             System.out.println();
         }
     }
 
-    private void move(){
-        System.out.print("CUBE> ");
+    private void move() {
+        System.out.print("\nCUBE> ");
         input = s.nextLine();
-        if(input.equalsIgnoreCase("U")){
-            moveUpL();
+
+        setDirec(input);
+
+        if(direction && input.charAt(0) == 'U'){
+            moveL(0);
+        }
+        if(!direction && input.charAt(0) == 'U'){
+            moveR(0);
+        }
+
+        if(direction && input.charAt(0) == 'B'){
+            moveL(2);
+        }
+        if(!direction && input.charAt(0) == 'B'){
+            moveR(2);
         }
 
         printCube();
     }
 
-    private void moveUpL() {
-        char temp = cube[0][0];
-
-        for(int i = 0; i < cube.length - 1; i++){
-            cube[0][i] = cube[0][i + 1];
+    private boolean setDirec(String input){
+        if (!input.contains("'")) {
+            direction = true;
         }
-        cube[0][2] = temp;
+        if (input.contains("'")) {
+            direction = false;
+        }
+
+        if (input.charAt(0) == 'L' || input.charAt(0)== 'B'){
+            direction = !direction;
+        }
+        return  direction;
+    }
+
+    private void moveL(int idx) {
+        char temp = cube[idx][0];
+
+        for (int i = 0; i < cube.length - 1; i++) {
+            cube[idx][i] = cube[idx][i + 1];
+        }
+        cube[idx][2] = temp;
+    }
+    
+    private void moveR(int idx){
+        char temp = cube[idx][cube[idx].length - 1];
+
+        for(int i = cube[idx].length - 1; i > 0; i--){
+            cube[idx][i] = cube[idx][i - 1];
+        }
+        cube[idx][0] = temp;
+    }
+
+    private void moveU(int idx){
+
+    }
+
+    private void moveD(int idx){
+
     }
 
     public static void main(String[] args) {
         FlatCube fc = new FlatCube();
+
     }
 }
