cceb3ae44a7760046c292b38ddeaa7ab99323ad7
Nayeon <kimnayeon0108@users.noreply.github.com>
2020-12-07 17:33:16 +0900

    Add move up to the left function

--- src/FlatCube.java
+++ src/FlatCube.java
@@ -5,10 +5,12 @@ public class FlatCube {
     private char[][] cube = {{'R', 'R', 'W'},
                         {'G', 'C', 'W'},
                         {'G', 'B', 'B'}};
+    private String input;
     private Scanner s = new Scanner(System.in);
 
     public FlatCube(){
         printCube();
+        move();
     }
 
     private void printCube(){
@@ -18,6 +20,25 @@ public class FlatCube {
             }
             System.out.println();
         }
+    }
+
+    private void move(){
+        System.out.print("CUBE> ");
+        input = s.nextLine();
+        if(input.equalsIgnoreCase("U")){
+            moveUpL();
+        }
+
+        printCube();
+    }
+
+    private void moveUpL() {
+        char temp = cube[0][0];
+
+        for(int i = 0; i < cube.length - 1; i++){
+            cube[0][i] = cube[0][i + 1];
+        }
+        cube[0][2] = temp;
     }
 
     public static void main(String[] args) {
