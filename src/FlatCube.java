91e5178d8c494e3acfae1e956499026b4d417b4c
Nayeon <kimnayeon0108@users.noreply.github.com>
2020-12-07 17:00:58 +0900

    Add print cube function

--- src/FlatCube.java
+++ src/FlatCube.java
@@ -1,2 +1,26 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class FlatCube {
+    private char[][] cube = {{'R', 'R', 'W'},
+                        {'G', 'C', 'W'},
+                        {'G', 'B', 'B'}};
+    private Scanner s = new Scanner(System.in);
+
+    public FlatCube(){
+        printCube();
+    }
+
+    private void printCube(){
+        for(int i = 0; i < cube.length; i++){
+            for(int j = 0; j < cube[i].length; j++){
+                System.out.print(cube[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        FlatCube fc = new FlatCube();
+    }
 }
