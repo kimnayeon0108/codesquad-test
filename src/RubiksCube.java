c9efedaa2bf87434965fac1fb67179b9e7c350ad
Nayeon <kimnayeon0108@users.noreply.github.com>
2020-12-08 20:43:07 +0900

    Add get cube array function

--- src/RubiksCube.java
+++ src/RubiksCube.java
@@ -1,2 +1,32 @@
 public class RubiksCube {
+    private char[][] cubeB = new char[3][3];
+    private char[][] cubeW = new char[3][3];
+    private char[][] cubeO = new char[3][3];
+    private char[][] cubeG = new char[3][3];
+    private char[][] cubeY = new char[3][3];
+    private char[][] cubeR = new char[3][3];
+
+    private void getCube(){
+        addElement(cubeB,'B');
+        addElement(cubeW,'W');
+        addElement(cubeO,'O');
+        addElement(cubeG,'G');
+        addElement(cubeY,'Y');
+        addElement(cubeR,'R');
+    }
+
+    private void addElement(char[][] cube, char a){
+        for(int i = 0; i < cube.length; i++){
+            for(int j = 0; j < cube[i].length; j++){
+                cube[i][j] = a;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        RubiksCube r = new RubiksCube();
+
+        r.getCube();
+
+    }
 }
