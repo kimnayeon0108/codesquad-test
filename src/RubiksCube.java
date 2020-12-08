f0dbefebf385a9e18a429de81024be612db47055
Nayeon <kimnayeon0108@users.noreply.github.com>
2020-12-08 22:38:40 +0900

    Modify print cube function

--- src/RubiksCube.java
+++ src/RubiksCube.java
@@ -8,24 +8,24 @@ public class RubiksCube {
                     addElement(z, 'B');
                     break;
                 case 1:
-                    addElement(z,'W');
+                    addElement(z, 'W');
                     break;
                 case 2:
-                    addElement(z,'O');
+                    addElement(z, 'O');
                     break;
                 case 3:
                     addElement(z, 'G');
                     break;
                 case 4:
-                    addElement(z,'Y');
+                    addElement(z, 'Y');
                     break;
                 case 5:
-                    addElement(z,'R');
+                    addElement(z, 'R');
                     break;
             }
         }
     }
-    
+
     private void addElement(int z, char a) {
         for (int i = 0; i < cube[z].length; i++) {
             for (int j = 0; j < cube[z][i].length; j++) {
@@ -35,14 +35,18 @@ public class RubiksCube {
     }
 
     private void printCube() {
-        for(int i = 0; i<cube.length; i++){
-            for(int j = 0; j < cube[i].length; j++){
-                for(int z = 0; z < cube[i][j].length; z++){
-                    System.out.print(cube[i][j][z] + " ");
-                }
-                System.out.println();
+        // B 면 출력
+        for (int j = 0; j < cube[0].length; j++) {
+            System.out.print("         ");
+            for (int z = 0; z < cube[0][j].length; z++) {
+                System.out.print(cube[0][j][z] + " ");
             }
+            System.out.println();
         }
+        System.out.println();
+
+        // W, O, G, Y 면 출력
+
     }
 
     public static void main(String[] args) {
