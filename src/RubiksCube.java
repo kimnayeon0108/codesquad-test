314aba607aa96d9d9e2859172e75ca47c943a4e4
Nayeon <kimnayeon0108@users.noreply.github.com>
2020-12-08 22:30:10 +0900

    Delete six kinds of arrays, create a three dimension array and modify add element function

--- src/RubiksCube.java
+++ src/RubiksCube.java
@@ -1,46 +1,54 @@
 public class RubiksCube {
-    private char[][] cubeB = new char[3][3];
-    private char[][] cubeW = new char[3][3];
-    private char[][] cubeO = new char[3][3];
-    private char[][] cubeG = new char[3][3];
-    private char[][] cubeY = new char[3][3];
-    private char[][] cubeR = new char[3][3];
+    private char[][][] cube = new char[6][3][3];
 
     private void getCube() {
-        addElement(cubeB, 'B');
-        addElement(cubeW, 'W');
-        addElement(cubeO, 'O');
-        addElement(cubeG, 'G');
-        addElement(cubeY, 'Y');
-        addElement(cubeR, 'R');
-
-        printCube();
-
+        for (int z = 0; z < cube.length; z++) {
+            switch (z) {
+                case 0:
+                    addElement(z, 'B');
+                    break;
+                case 1:
+                    addElement(z,'W');
+                    break;
+                case 2:
+                    addElement(z,'O');
+                    break;
+                case 3:
+                    addElement(z, 'G');
+                    break;
+                case 4:
+                    addElement(z,'Y');
+                    break;
+                case 5:
+                    addElement(z,'R');
+                    break;
+            }
+        }
     }
-
-    private void addElement(char[][] cube, char a) {
-        for (int i = 0; i < cube.length; i++) {
-            for (int j = 0; j < cube[i].length; j++) {
-                cube[i][j] = a;
+    
+    private void addElement(int z, char a) {
+        for (int i = 0; i < cube[z].length; i++) {
+            for (int j = 0; j < cube[z][i].length; j++) {
+                cube[z][i][j] = a;
             }
         }
     }
 
     private void printCube() {
-        for (int i = 0; i < cubeB.length; i++) {
-            for (int j = 0; j < cubeB[i].length; j++) {
-                System.out.print(cubeB[i][j] + " ");
+        for(int i = 0; i<cube.length; i++){
+            for(int j = 0; j < cube[i].length; j++){
+                for(int z = 0; z < cube[i][j].length; z++){
+                    System.out.print(cube[i][j][z] + " ");
+                }
+                System.out.println();
             }
-            System.out.println();
         }
-        System.out.println();
     }
 
     public static void main(String[] args) {
         RubiksCube r = new RubiksCube();
-
         r.getCube();
-
+        r.printCube();
 
     }
 }
